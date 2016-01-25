b4803462c5db74b781004ce9f67b972caf77451f
Jonathan Lu <jonathanlu@Jonathans-MacBook-Pro.local>
2016-01-24 17:11:02 -0800

    Finalized commit for Sprint 1

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-var express = require('express'),
-    server = express.createServer();
+var express = require('express');
+
+var app = express();
+var server = http.createServer(app);
 
 server.use('/frameworks/cocos2d-html5', express.static(__dirname + '/frameworks/cocos2d-html5'));
 server.use('/src', express.static(__dirname + '/src'));
 server.use('/res', express.static(__dirname + '/res'));
 server.use('/', express.static(__dirname));
 
-server.get('/', function(req, res){
-    console.log('Here we go');
-});
-
-server.get('/api/hello', function(req, res){
-   res.send('Hello Cruel World');
+server.get('/', function(req, res) {
 });
 
 server.listen(process.env.PORT || 8081);
