9bc8a0c1d327980b493435585fd83b96387358e1
Brett Hatch <bhatch@ucsc.edu>
2016-02-17 18:43:55 -0800

    added random jumbo mode!

--- src/GameConfig.js
+++ src/GameConfig.js
@@ -81,12 +81,14 @@ NJ.saveSettings = function() {
 NJ.gameState = {
     currentJumboId: "",
     currentLevel: 1,
-    multiplier: 1
+    multiplier: 1,
+    randomJumbo: false
 };
 
 NJ.resetGameState = function() {
     NJ.gameState.currentLevel = 1;
     NJ.gameState.multiplier = 1;
+    NJ.gameState.randomJumbo = false;
 };
 
 // Use this function to set the current jumbo. DO NOT read currentJumboId directly!!!
