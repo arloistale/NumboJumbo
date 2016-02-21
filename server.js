0630b022827f0531d91fed2538bd929a758e4a9e
arloistale <jonathanlu321@gmail.com>
2016-02-21 01:13:31 -0800

    Removed unnecessary uses in node app

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express');
 var app = express();
 
 app.configure(function() {
-    app.use('/frameworks/cocos2d-html5', express.static(__dirname + '/frameworks/cocos2d-html5'));
     app.use('/src', express.static(__dirname + '/src'));
     app.use('/res', express.static(__dirname + '/res'));
     app.use('/public', express.static(__dirname + '/public'));
