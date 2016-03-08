3fbb5ff20d860947458d3f1d001e062d73ffefc4
arloistale <jonathanlu321@gmail.com>
2016-03-08 15:41:25 -0800

    Reintroduced orderless combos

--- src/NumboController.js
+++ src/NumboController.js
@@ -352,37 +352,24 @@ var NumboController = cc.Class.extend({
 
 		var selectedBlocksLength = this._selectedBlocks.length;
 
-		// all blocks must be sequentially adjacent
-
-		/*
 		var sum = 0;
+		var max = 0;
 
-		for (var i = 0; i < selectedBlocksLength - 1; ++i) {
-		    if (!this._numboLevel.isAdjBlocks(this._selectedBlocks[i], this._selectedBlocks[i + 1]))
-			return false;
+		var i = 0;
 
-		    sum += this._selectedBlocks[i].val;
+		for(; i < selectedBlocksLength - 1; ++i) {
+			if(!this._numboLevel.isAdjBlocks(this._selectedBlocks[i], this._selectedBlocks[i + 1]))
+				return false;
 		}
 
-		return sum == this._selectedBlocks[selectedBlocksLength - 1].val;
-		*/
-
-		var sum=0;
-		//var max=0;
-		/*
-		for(var i=0; i<selectedBlocksLength-1; ++i) {
-			if(!this._numboLevel.isAdjBlocks(this._selectedBlocks[i], this._selectedBlocks[i+1]))
-				return false;
-		}*/
-		for(var i=0; i<selectedBlocksLength - 1; ++i) {
-			if (!this._numboLevel.isAdjBlocks(this._selectedBlocks[i], this._selectedBlocks[i + 1]))
-				return false;
-
+		for(i = 0; i < selectedBlocksLength; ++i)
 			sum += this._selectedBlocks[i].val;
-			//max = Math.max(this._selectedBlocks[i].val, max);
-		}
-		//sum -= max;
 
-		return sum == this._selectedBlocks[selectedBlocksLength - 1].val;
+		for(i = 0; i <selectedBlocksLength; ++i) {
+			if(sum - this._selectedBlocks[i].val == this._selectedBlocks[i].val)
+				return true;
+		}
+
+		return false;//sum == this._selectedBlocks[selectedBlocksLength - 1].val;
 	}
 });
